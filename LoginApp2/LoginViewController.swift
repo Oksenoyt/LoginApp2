49d571a238bde900bad537bcbe73f6fe968bc24e
Oksenoyt <a.oksenoyt@gmail.com>
2022-07-01 20:31:14 +0300

    added second view

--- LoginApp2/LoginViewController.swift
+++ LoginApp2/LoginViewController.swift
@@ -18,10 +18,31 @@ class LoginViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
 
-    @IBAction func loginAction() {
-        let user = userTextField.text ?? ""
-        let password = passwordTextField.text ?? ""
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        guard let welcomeVC = segue.destination as? WelcomeViewController else { return }
+        welcomeVC.greetings = userTextField.text
     }
-    
+
+    @IBAction func loginAction() {
+        guard userTextField.text == "qwe" else {
+            print("wrong login or password")
+            showAlert(title: "Wrong login or password", message: "Please enter correct username and password")
+            return
+            }
+        guard passwordTextField.text == "asd" else {
+            print("wrong login or password")
+            showAlert(title: "Wrong login or password", message: "Please enter correct username and password")
+            return
+            }
+    }
 }
 
+//MARK - UIAlertController
+extension LoginViewController {
+    private func showAlert(title: String, message: String) {
+        let alert = UIAlertController(title: title, message: message, preferredStyle: .alert)
+        let okAction = UIAlertAction(title: "OK", style: .default) { _ in self.passwordTextField.text = ""}
+        alert.addAction(okAction)
+        present(alert, animated: true)
+    }
+}
